2931499a83d528d936c7c2c1090e7efd0d6afb64
luca granato <lucagranatostudio@gmail.com>
2024-12-02 15:14:15 +0100

    1

--- JavaScript/esercizio1/D1.js
+++ JavaScript/esercizio1/D1.js
@@ -7,9 +7,9 @@ REGOLE
 - Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
 */
 
-/* ESERCIZIO 1
- Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
-*/
+// ESERCIZIO 1
+
+console.log('Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.')
 
 let stringa = 'testo';
 let numero = 1;
@@ -17,48 +17,46 @@ let numeroDecimale = 3.14
 let boolean = true;
 let vuoto = null;
 
-/* ESERCIZIO 2
- Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
-*/
+console.log(stringa, numero, numeroDecimale, boolean, vuoto);
+
+// ESERCIZIO 2
+
+ console.log('Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.');
 
 let myName = 'Luca';
   console.log(myName); 
 
-/* ESERCIZIO 3
- Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
-*/
+// ESERCIZIO 3
+
+console.log('Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.');
 
 let a = 4;
 let b = 6;
   console.log(a + b);
 
-/* ESERCIZIO 4
- Crea una variable di nome "x" e assegna ad essa il numero 12.
-*/
+// ESERCIZIO 4
+ console.log('Crea una variable di nome "x" e assegna ad essa il numero 12.');
 
 let x = 12;
   console.log(x);
 
-/* ESERCIZIO 5
-  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
-  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
-*/
+// ESERCIZIO 5
+  console.log('Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.')
+  console.log('Dimostra l &#39; impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.');
 
 myName = 'Granato';
 // const myName = Luca;
   console.log(myName);
 
-/* ESERCIZIO 6
- Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
-*/
+// ESERCIZIO 6
+  console.log('Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).');
 
   console.log(4 - x);
 
-/* ESERCIZIO 7
- Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
- Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
- EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
-*/
+// ESERCIZIO 7
+ console.log('Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).');
+ console.log('Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).');
+ console.log('EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).');
 
 let name1 = 'john';
 let name2 = 'John';
